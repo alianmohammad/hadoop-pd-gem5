a4d05eb04ca064389932a4e58122450bba933515
Mohammad Alian <alian-admin@clara-18.ece.wisc.edu>
2015-04-29 11:58:05 -0500

    fixed bug in script clean

--- script_clean.sh
+++ script_clean.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-rm -rf $HADOOP_HOME/logs
+rm -rf /users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/logs
 rm -rf /users/alian-admin/temps
 mkdir /users/alian-admin/temps 
 mkdir /users/alian-admin/temps/tmp0 
 mkdir /users/alian-admin/temps/tmp1
 mkdir /users/alian-admin/temps/tmp2 
 
-ssh clara-12 'rm -rf $HADOOP_HOME/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
-ssh clara-13 'rm -rf $HADOOP_HOME/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
-ssh clara-14 'rm -rf $HADOOP_HOME/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
-ssh clara-15 'rm -rf $HADOOP_HOME/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
+ssh clara-12 'rm -rf /users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
+ssh clara-13 'rm -rf /users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
+ssh clara-14 'rm -rf /users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
+ssh clara-15 'rm -rf /users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/logs;rm -rf /users/alian-admin/temps;mkdir /users/alian-admin/temps;mkdir /users/alian-admin/temps/tmp0;mkdir /users/alian-admin/temps/tmp1;mkdir /users/alian-admin/temps/tmp2'
 
