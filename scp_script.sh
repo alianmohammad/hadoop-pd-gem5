7daa0df27ad31dcbe0a93b21ce28bbe20ec72ebe
Mohammad Alian <alian-admin@clara-18.ece.wisc.edu>
2015-04-30 21:27:22 -0500

    scp and stop scripts added

---
+++ scp_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scp -r hadoop-1.0.2/build clara-12:/users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/
+scp -r hadoop-1.0.2/build clara-13:/users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/
+scp -r hadoop-1.0.2/build clara-14:/users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/
+scp -r hadoop-1.0.2/build clara-15:/users/alian-admin/hadoop-pdgem5/hadoop-1.0.2/
